c9ef2a6a7f396403ffd15d9df44b54f8f02e5f75
Dileep Jose <dileep.jose@gmail.com>
2018-03-01 20:23:34 -0800

    First Commit

---
+++ src/main/java/com/user/registration/service/UserService.java
@@ -0,0 +1,27 @@
+package com.user.registration.service;
+
+import java.util.List;
+
+import com.user.registration.model.User;
+
+
+
+public interface UserService {
+	
+	User findById(long id);
+	
+	User findByName(String name);
+	
+	void saveUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUserById(long id);
+
+	List<User> findAllUsers(); 
+	
+	void deleteAllUsers();
+	
+	public boolean isUserExist(User user);
+	
+}
